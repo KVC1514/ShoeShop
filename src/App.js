5971cac16f3a6506027caa53e636a8d882b85d88
Kundan Chaudhary <kundanchaudhary@Kundans-MacBook-Pro.local>
2024-08-17 00:39:36 +0100

    all button not working and search bar not working

--- src/App.js
+++ src/App.js
@@ -80,8 +80,8 @@ function App() {
   return (
     <>
       <Sidebar handleChange={handleChange} />
-      <Navigation query={query} handleChange={handleChange} />
-      <Recommended handleChange={handleClick} />
+      <Navigation query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
       <Products result={result} />
     </>
   );
--- src/SideBar/Colors/Colors.js
+++ src/SideBar/Colors/Colors.js
@@ -7,7 +7,7 @@ function Colors({ handleChange }) {
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
       <lable className="sidebar-label-container">
-        <input onChange={handleChange} type="radio" value="" name="test2" />
+        <input onChange={handleChange} type="radio" value="" name="test3" />
         <span className="checkmark all"></span>All
       </lable>
 
@@ -15,28 +15,28 @@ function Colors({ handleChange }) {
         handleChange={handleChange}
         value="black"
         title="Black"
-        name="test1"
+        name="test3"
         color="black"
       />
       <Input
         handleChange={handleChange}
         value="blue"
         title="Blue"
-        name="test1"
+        name="test3"
         color="blue"
       />
       <Input
         handleChange={handleChange}
         value="red"
         title="Red"
-        name="test1"
+        name="test3"
         color="red"
       />
       <Input
         handleChange={handleChange}
         value="green"
         title="Green"
-        name="test1"
+        name="test3"
         color="green"
       />
       <label className="sidebar-label-container">
@@ -44,7 +44,7 @@ function Colors({ handleChange }) {
           type="radio"
           onChange={handleChange}
           value="White"
-          name="test1"
+          name="test3"
         />
         <span
           className="checkmark"
